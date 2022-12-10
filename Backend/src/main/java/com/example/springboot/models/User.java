dd0e92e624314d1b346180343dc28bea138a5837
BudiGabe <razvan.gabriel.budaca@gmail.com>
2022-12-10 20:39:32 +0200

    Users can no longer have duplicate names

--- Backend/src/main/java/com/example/springboot/models/User.java
+++ Backend/src/main/java/com/example/springboot/models/User.java
@@ -15,7 +15,7 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", unique = true)
     private String name;
 
     @Column(name = "password")
