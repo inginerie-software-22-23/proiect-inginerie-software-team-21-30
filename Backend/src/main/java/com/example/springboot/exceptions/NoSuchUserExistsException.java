4e16cb93d1270e6a24b3db14810b496d119b2107
BudiGabe <razvan.gabriel.budaca@gmail.com>
2022-12-13 17:29:42 +0200

    Implemented exception for existing user on register

---
+++ Backend/src/main/java/com/example/springboot/exceptions/NoSuchUserExistsException.java
@@ -0,0 +1,13 @@
+package com.example.springboot.exceptions;
+
+public class NoSuchUserExistsException extends RuntimeException {
+    private String message;
+
+    public NoSuchUserExistsException() {
+    }
+
+    public NoSuchUserExistsException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
