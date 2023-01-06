ea84e17292e073d817a1f50dda430ddc5ef391d3
Alexandru Sasu <sasualex1@gmail.com>
2023-01-06 15:46:14 +0200

    Second fix for user roles on BE

--- Backend/src/main/java/com/example/springboot/models/Role.java
+++ Backend/src/main/java/com/example/springboot/models/Role.java
@@ -1,5 +1,6 @@
 package com.example.springboot.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,5 +22,6 @@ public class Role {
     private String name;
 
     @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
     private Collection<User> users;
 }
