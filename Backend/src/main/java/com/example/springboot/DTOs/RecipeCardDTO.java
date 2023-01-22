76810b00636d169240f932b1a3fc0199caff15a0
Alexandru Sasu <sasualex1@gmail.com>
2023-01-22 17:33:43 +0200

    Added user for retrieval of recipe cards details

--- Backend/src/main/java/com/example/springboot/DTOs/RecipeCardDTO.java
+++ Backend/src/main/java/com/example/springboot/DTOs/RecipeCardDTO.java
@@ -6,12 +6,14 @@ import lombok.Data;
 @Data
 public class RecipeCardDTO {
     private Long id;
+    private UserDTO author;
     private String name;
     private byte[] image;
     private Integer estimatedPrepTimeInMinutes;
 
     public RecipeCardDTO(Recipe recipe) {
         this.id = recipe.getId();
+        this.author = new UserDTO(recipe.getUser());
         this.name = recipe.getName();
         this.image = recipe.getImage();
         this.estimatedPrepTimeInMinutes = recipe.getEstimatedPrepTimeInMinutes();
